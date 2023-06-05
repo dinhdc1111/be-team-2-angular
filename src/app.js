a9936ac2a97c9b3ce5b1d523ceb09d5d815219c7
Ducvpop <ducnhph22018@fpt.edu.vn>
2023-06-05 22:26:47 +0700

    feat:TEAM2-3-Category-Api refs:TEAM2-3

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import authRouter from "./routes/auth";
 import mongoose from "mongoose";
+import RouterCategories from "./routes/category";
 
 // Config
 dotenv.config();
@@ -16,7 +17,7 @@ app.use(morgan("tiny"));
 // connect
 
 app.use("/api", authRouter);
-
+app.use("/api", RouterCategories);
 mongoose
   .connect("mongodb://127.0.0.1:27017/nodejs-angular")
   .then(() => {
